3053647a362c3195198817e0f7586a19ba41c817
ElenaKuzmina <74042287+ElenaKuzmina@users.noreply.github.com>
2022-11-28 10:53:28 +0300

    Add files via upload

---
+++ SF2022User00LibUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SF2022User00LibUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SF2022User00LibUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b2fbf532-48ca-4cda-ac7f-e3b96cf19897")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SF2022User00LibUnitTest/UnitTestSF2022.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SF2022User00Lib;
+
+namespace SF2022User00LibUnitTest
+{
+    [TestClass]
+    public class UnitTestSF2022
+    {
+        Calculations calc;
+
+        [TestMethod]
+        public void TestMethodIsNotNull()
+        {
+            calc = new Calculations();
+            Assert.IsNotNull(calc);
+
+        }
+        [TestMethod]
+        public void TestMethodAllIntervals()
+        {
+            TimeSpan[] interval = new TimeSpan[]{new TimeSpan(10,0,0),
+                new TimeSpan(11,0,0),
+                new TimeSpan(15,0,0),
+                new TimeSpan(15,30,0),
+                new TimeSpan(16,50,0) };
+            int[] dur = new int[] { 60, 30, 10, 10, 40 };
+            calc = new Calculations();
+            string[] result = calc.AvailablePeriods(
+               interval, dur, new TimeSpan(8, 0, 0),
+               new TimeSpan(18, 0, 0), 30);
+            string[] expected = new string[] { "08:00 - 08:30", "08:30 - 09:00",
+            "09:00 - 09:30", "09:30 - 10:00", "11:30 - 12:00", "12:00 - 12:30",
+            "12:30 - 13:00", "13:00 - 13:30", "13:30 - 14:00", "14:00 - 14:30",
+            "14:30 - 15:00", "15:40 - 16:10", "16:10 - 16:40","17:30 - 18:00" };
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            TimeSpan[] interval = new TimeSpan[]{new TimeSpan(10,0,0),
+                new TimeSpan(11,0,0),
+                new TimeSpan(15,0,0),
+                new TimeSpan(15,30,0),
+                new TimeSpan(16,50,0) };
+            int[] dur = new int[] { 60, 30, 10, 10, 40 };
+            calc = new Calculations();
+            string[] result = calc.AvailablePeriods(
+               interval, dur, new TimeSpan(8, 0, 0),
+               new TimeSpan(18, 0, 0), 30);
+            string[] expected = new string[] {"08:00-08:30", "08:30-09:00",
+            "09:00-09:30", "09:30-10:00","10:00-10:30","10:30-11:00",
+            "11:00-11:30","11:30-12:00","12:00-12:30","12:30-13:00",
+            "13:00-13:30","13:30-14:00","14:00-14:30","14:30-15:00",
+            "15:00-15:30","15:30-16:00","16:00-16:30","16:30-17:00",
+            "17:00-17:30","17:30-18:00" };
+            CollectionAssert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+        }
+    }
+}
